4ada15bfeb60bcf6cfddcc221710205501b1887e
Dariusz Antoniuk <qaston@gmail.com>
2023-02-24 21:29:48 +0000

    Update to new actix. This is horrible

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,21 @@
-
-// Based on:
-//   https://github.com/DoumanAsh/actix-reverse-proxy
-//   https://golang.org/src/net/http/httputil/reverseproxy.go
+//! Reverse proxy specifically for purposes of using a node dev server with the backend
+//!
+//! Based on:
+//! <https://github.com/DoumanAsh/actix-reverse-proxy>
+//! <https://golang.org/src/net/http/httputil/reverseproxy.go>
+//! <https://github.com/felipenoris/actix-reverse-proxy>
 
 extern crate actix_web;
-extern crate futures;
 
-#[macro_use]
-extern crate lazy_static;
+use actix_web::http::header::{HeaderMap, HeaderName};
+use actix_web::web::Bytes;
+use actix_web::{HttpRequest, HttpResponse};
 
-use actix_web::{HttpRequest, HttpResponse, HttpMessage, client};
-use actix_web::http::header::{HeaderName, HeaderMap};
-use futures::{Stream, Future};
-
-use std::time::Duration;
+use std::collections::HashSet;
 use std::net::SocketAddr;
+use std::time::Duration;
 
-#[cfg(test)]
-mod tests;
-
-lazy_static! {
+lazy_static::lazy_static! {
     static ref HEADER_X_FORWARDED_FOR: HeaderName = HeaderName::from_lowercase(b"x-forwarded-for").unwrap();
 
     static ref HOP_BY_HOP_HEADERS: Vec<HeaderName> = vec![
@@ -42,8 +38,9 @@ lazy_static! {
 static DEFAULT_TIMEOUT: Duration = Duration::from_secs(60);
 
 pub struct ReverseProxy<'a> {
-    forward_url: &'a str,
+    forward_host: &'a str,
     timeout: Duration,
+    response_size: usize
 }
 
 fn add_client_ip(fwd_header_value: &mut String, client_addr: SocketAddr) {
@@ -56,39 +53,47 @@ fn add_client_ip(fwd_header_value: &mut String, client_addr: SocketAddr) {
 }
 
 fn remove_connection_headers(headers: &mut HeaderMap) {
-    let mut headers_to_delete: Vec<String> = Vec::new();
     let header_connection = &(*HEADER_CONNECTION);
 
-    if headers.contains_key(header_connection) {
-        if let Ok(connection_header_value) = headers[header_connection].to_str() {
-            for h in connection_header_value.split(',').map(|s| s.trim()) {
-                headers_to_delete.push(String::from(h));
-            }
-        }
-    }
-
-    for h in headers_to_delete {
+    let connection_headers: HashSet<HeaderName> = headers
+        .get_all(header_connection)
+        .map(|v| HeaderName::from_bytes(v.as_bytes()).unwrap())
+        .collect();
+    for h in connection_headers {
         headers.remove(h);
     }
 }
 
 fn remove_request_hop_by_hop_headers(headers: &mut HeaderMap) {
     for h in HOP_BY_HOP_HEADERS.iter() {
-        if headers.contains_key(h) && (headers[h] == "" || ( h == *HEADER_TE && headers[h] == "trailers")  ) {
-            continue;
+        match headers.get(h) {
+            Some(v) => {
+                if v == "" || (h == *HEADER_TE && v == "trailers") {
+                    continue;
+                }
+                headers.remove(h);
+            }
+            None => continue,
         }
-        headers.remove(h);
     }
 }
 
 impl<'a> ReverseProxy<'a> {
-
-    pub fn new(forward_url: &'a str) -> ReverseProxy<'a> {
-        ReverseProxy{ forward_url, timeout: DEFAULT_TIMEOUT }
+    pub fn new(forward_host: &'a str) -> ReverseProxy<'a> {
+        ReverseProxy {
+            forward_host,
+            timeout: DEFAULT_TIMEOUT,
+            response_size: 2 * 1024 * 1024,
+        }
     }
 
     pub fn timeout(mut self, duration: Duration) -> ReverseProxy<'a> {
         self.timeout = duration;
+        self
+    }
+
+    pub fn response_size(mut self, response_size: usize) -> ReverseProxy<'a> {
+        self.response_size = response_size;
         self
     }
 
@@ -112,66 +117,74 @@ impl<'a> ReverseProxy<'a> {
         result
     }
 
-    fn forward_uri(&self, req: &HttpRequest) -> String {
-        let forward_url: &str = self.forward_url;
+    fn forward_uri(&self, path: &str, req: &HttpRequest) -> String {
+        let forward_host: &str = self.forward_host;
 
         let forward_uri = match req.uri().query() {
-            Some(query) => format!("{}{}?{}", forward_url, req.uri().path(), query),
-            None => format!("{}{}", forward_url, req.uri().path()),
+            Some(query) => format!("{}{}?{}", forward_host, path, query),
+            None => format!("{}{}", forward_host, path),
         };
 
         forward_uri
     }
 
-    pub fn forward(&self, req: HttpRequest) -> impl Future<Item=actix_web::HttpResponse, Error=actix_web::Error>  {
+    pub async fn forward(&self,
+        req: &HttpRequest,
+        body: Bytes,
+    ) -> Result<actix_web::HttpResponse, actix_web::error::Error> {
+        self.forward_path(req, req.uri().path(), body).await
+    }
 
-        let mut forward_req = client::ClientRequest::build_from(&req);
-        forward_req.uri(self.forward_uri(&req).as_str());
-        forward_req.set_header( &(*HEADER_X_FORWARDED_FOR), self.x_forwarded_for_value(&req));
+    pub async fn forward_path(
+        &self,
+        req: &HttpRequest,
+        forward_path: &str,
+        body: Bytes,
+    ) -> Result<actix_web::HttpResponse, actix_web::error::Error> {
+        let client = awc::Client::new();
 
-        let forward_body = req.payload().from_err();
-        let mut forward_req = forward_req
-                                    .no_default_headers()
-                                    .set_header_if_none(actix_web::http::header::USER_AGENT, "")
-                                    .body(actix_web::Body::Streaming(Box::new(forward_body)))
-                                    .expect("To create valid forward request");
+        // set headers from the client instead of default headers
+        let mut forward_req = client
+            .request_from(self.forward_uri(forward_path, &req).as_str(), req.head())
+            .insert_header((
+                HEADER_X_FORWARDED_FOR.clone(),
+                self.x_forwarded_for_value(&req),
+            ))
+            .insert_header_if_none((actix_web::http::header::USER_AGENT, ""));
 
         remove_connection_headers(forward_req.headers_mut());
         remove_request_hop_by_hop_headers(forward_req.headers_mut());
 
-        println!("#### REVERSE PROXY REQUEST HEADERS");
+        tracing::debug!("#### REVERSE PROXY REQUEST HEADERS");
         for (key, value) in forward_req.headers() {
-            println!("[{:?}] = {:?}", key, value);
+            tracing::debug!("[{:?}] = {:?}", key, value);
         }
 
-        forward_req.send()
-                    .timeout(self.timeout)
-                    .map_err(|error| {
-                        println!("Error: {}", error);
-                        error.into()
-                    })
-                    .map(|resp| {
-                        let mut back_rsp = HttpResponse::build(resp.status());
+        let mut resp = forward_req
+            .timeout(self.timeout)
+            .send_body(body)
+            .await
+            .map_err(|e| actix_web::error::ErrorBadGateway(e))?;
 
-                        // copy headers
-                        for (key, value) in resp.headers() {
-                            if !HOP_BY_HOP_HEADERS.contains(key) {
-                                back_rsp.header(key.clone(), value.clone());
-                            }
-                        }
+        let mut back_rsp = HttpResponse::build(resp.status());
 
-                        let back_body = resp.payload().from_err();
-                        let mut back_rsp = back_rsp
-                            .body(actix_web::Body::Streaming(Box::new(back_body)));
+        let header_connection = &(*HEADER_CONNECTION);
 
-                        remove_connection_headers(back_rsp.headers_mut());
-
-                        println!("#### REVERSE PROXY RESPONSE HEADERS");
-                        for (key, value) in back_rsp.headers() {
-                            println!("[{:?}] = {:?}", key, value);
-                        }
-
-                        back_rsp
-                    })
+        let mut connection_headers: HashSet<HeaderName> = resp
+            .headers()
+            .get_all(header_connection)
+            .map(|v| HeaderName::from_bytes(v.as_bytes()).unwrap())
+            .collect();
+        connection_headers.insert(HeaderName::from_lowercase(b"content-encoding").unwrap());
+        // copy headers
+        tracing::debug!("#### REVERSE PROXY RESPONSE HEADERS");
+        for (key, value) in resp.headers() {
+            if !HOP_BY_HOP_HEADERS.contains(key) && !connection_headers.contains(key) {
+                back_rsp.append_header((key.clone(), value.clone()));
+                tracing::debug!("[{:?}] = {:?}", key, value);
+            }
+        }
+        let resp_body = resp.body().limit(self.response_size).await?;
+        Ok(back_rsp.body(resp_body))
     }
 }
